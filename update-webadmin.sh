5b31cb10ea08e9076d674f865e7fcde2d5d557ef
askmeaboutlo0m <askmeaboutlo0m@users.noreply.github.com>
2023-03-21 17:58:07 +0100

    Add NODE_OPTIONS=--openssl-legacy-provider (#5)
    To the webadmin build script. New node versions require it, otherwise it
    dies with `ERR_OSSL_EVP_UNSUPPORTED`. A more proper solution would be to
    update the webadmin to a newer React version, but that's a lot of work
    no one has time for currently.

--- update-webadmin.sh
+++ update-webadmin.sh
@@ -25,6 +25,7 @@ docker run --rm -ti \
 	--mount type=bind,source="$(pwd)",target=/app \
 	-w=/app \
 	-u=$UID \
+	-e 'NODE_OPTIONS=--openssl-legacy-provider' \
 	node:lts-alpine \
 	/bin/sh -c "npm install --no-progress && npm run build"
 
