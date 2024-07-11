8ec3a6d1b2a31410c51b17db04c05e8248a3d329
MorrowShore <97566823+MorrowShore@users.noreply.github.com>
2024-07-11 19:58:42 +0330

    Update setup.ubuntu.sh

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-packages=(apache2-utils curl dialog docker.io docker-compose git)
+
+if [[ "$(lsb_release -rs | perl -pe 's/\..+//s')" -gt 20 ]]; then
+    packages=(apache2-utils curl dialog docker.io docker-compose-v2 git)
+else
+    packages=(apache2-utils curl dialog docker.io docker-compose git)
+fi
+
 if [[ $EUID -eq 0 ]]; then
     set -x
     apt update && apt -y install "${packages[@]}"
@@ -7,4 +13,3 @@ else
     set -x
     sudo apt update && sudo apt -y install "${packages[@]}"
 fi
-
