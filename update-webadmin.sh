f954e3c0720a7aae5ca9a7720343d6a3e4f6e94a
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2025-03-28 02:47:03 +0100

    Update dpwebadmin to 0.11.0

--- update-webadmin.sh
+++ update-webadmin.sh
@@ -2,7 +2,7 @@
 set -ex
 cd "$(dirname "$0")"
 
-version='0.4.0'
+version='0.11.0'
 curl -L "https://github.com/drawpile/dpwebadmin/releases/download/$version/dpwebadmin-$version.tar.gz" | tar xz
 
 rm -rf public_html/admin
